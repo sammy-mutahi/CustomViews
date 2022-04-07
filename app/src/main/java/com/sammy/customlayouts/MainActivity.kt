126f1011a038b5a62f91ce0190c2038536f844d4
sammy-mutahi <sammymutahi21@gmail.com>
2022-04-07 12:06:41 +0300

    basic shapes

--- app/src/main/java/com/sammy/customlayouts/MainActivity.kt
+++ app/src/main/java/com/sammy/customlayouts/MainActivity.kt
@@ -3,13 +3,24 @@ package com.sammy.customlayouts
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.geometry.Size
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.drawscope.Stroke
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.sammy.customlayouts.ui.theme.CustomLayoutsTheme
 
 class MainActivity : ComponentActivity() {
@@ -17,21 +28,66 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             CustomLayoutsTheme {
-
+                MyCanvas()
             }
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MyCanvas(){
+    Canvas(modifier = Modifier
+        .padding(20.dp)
+        .size(300.dp)){
+        drawRect(
+            color = Color.Black,
+            size = size
+        )
+        drawRect(
+            color = Color.Red,
+            topLeft = Offset(100f,100f),
+            size = Size(100f, 100f),
+            style = Stroke(
+                width = 3.dp.toPx(),
+            )
+        )
+        drawCircle(
+            brush = Brush.radialGradient(
+                colors = listOf(Color.Red,Color.Yellow),
+                center = center,
+                radius = 100f
+            ),
+            radius = 100f,
+        )
+        drawArc(
+            color = Color.Green,
+            startAngle = 0f,
+            sweepAngle = 270f,
+            useCenter = false,
+            topLeft = Offset(100f,500f),
+            size = Size(200f,200f),
+            style = Stroke(
+                width = 3.dp.toPx()
+            )
+        )
+        drawOval(
+            color = Color.Magenta,
+            topLeft = Offset(500f,100f),
+            size = Size(200f,300f)
+        )
+        drawLine(
+            color = Color.Cyan,
+            start = Offset(300f,700f),
+            end = Offset(700f,700f),
+            strokeWidth = 3.dp.toPx()
+        )
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CustomLayoutsTheme {
-        Greeting("Android")
+        MyCanvas()
     }
 }
