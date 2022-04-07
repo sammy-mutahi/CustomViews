fa16ba5f7eaba9162e010159dbdb3e7c6e5ef8da
sammy-mutahi <sammymutahi21@gmail.com>
2022-04-07 11:41:21 +0300

    create project

---
+++ app/src/main/java/com/sammy/customlayouts/MainActivity.kt
@@ -0,0 +1,37 @@
+package com.sammy.customlayouts
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.sammy.customlayouts.ui.theme.CustomLayoutsTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CustomLayoutsTheme {
+
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    CustomLayoutsTheme {
+        Greeting("Android")
+    }
+}
