0f91b8dee10260dff866f76d377482ff13178d17
sammy-mutahi <sammymutahi21@gmail.com>
2022-04-07 19:11:55 +0300

    draw text on canvas

--- app/src/main/java/com/sammy/customlayouts/MainActivity.kt
+++ app/src/main/java/com/sammy/customlayouts/MainActivity.kt
@@ -1,14 +1,18 @@
 package com.sammy.customlayouts
 
+import android.graphics.Color
+import android.graphics.Paint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Canvas
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.nativeCanvas
 import androidx.compose.ui.tooling.preview.Preview
 import com.sammy.customlayouts.ui.theme.CustomLayoutsTheme
 
@@ -24,14 +28,26 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun DrawText() {
+    Canvas(modifier = Modifier.fillMaxSize()){
+        drawContext.canvas.nativeCanvas.apply {
+            drawText(
+                "This is my canvas text",
+                100f,
+                100f,
+                Paint().apply {
+                    color = Color.RED
+                    textSize = 100f
+                }
+            )
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     CustomLayoutsTheme {
-        Greeting("Android")
+        DrawText()
     }
 }
