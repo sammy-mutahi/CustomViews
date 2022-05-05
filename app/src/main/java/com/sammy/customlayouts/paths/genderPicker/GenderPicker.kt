ee2ed672ab7c95eb701255e2054a2f161d0b22fc
sammy-mutahi <sammymutahi21@gmail.com>
2022-05-05 18:30:14 +0300

    add gender picker class

---
+++ app/src/main/java/com/sammy/customlayouts/paths/genderPicker/GenderPicker.kt
@@ -0,0 +1,14 @@
+package com.sammy.customlayouts.paths.genderPicker
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class GenderPicker : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
