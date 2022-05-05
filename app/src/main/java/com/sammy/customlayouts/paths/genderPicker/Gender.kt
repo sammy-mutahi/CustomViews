b5f7a7d7b5bfa3cb43e9758f310bf085e56d7ef0
sammy-mutahi <sammymutahi21@gmail.com>
2022-05-06 00:12:44 +0300

    add gender picker

---
+++ app/src/main/java/com/sammy/customlayouts/paths/genderPicker/Gender.kt
@@ -0,0 +1,6 @@
+package com.sammy.customlayouts.paths.genderPicker
+
+sealed class Gender {
+    object Male : Gender()
+    object Female : Gender()
+}
--- app/src/main/java/com/sammy/customlayouts/paths/genderPicker/GenderPicker.kt
+++ app/src/main/java/com/sammy/customlayouts/paths/genderPicker/GenderPicker.kt
@@ -3,12 +3,24 @@ package com.sammy.customlayouts.paths.genderPicker
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.ui.Modifier
+import com.sammy.customlayouts.ui.theme.CustomLayoutsTheme
 
 class GenderPicker : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
+            CustomLayoutsTheme {
+                Column {
+                    GenderPickerComponent(
+                        modifier = Modifier.fillMaxSize()
+                    ) {
 
+                    }
+                }
+            }
         }
     }
 }
