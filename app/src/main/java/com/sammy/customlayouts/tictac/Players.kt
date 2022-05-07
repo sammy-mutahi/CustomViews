b9c79e5b8a1992e76ef8f37cf1566c8c50eb396c
sammy-mutahi <sammymutahi21@gmail.com>
2022-05-07 12:42:12 +0300

    add players model

---
+++ app/src/main/java/com/sammy/customlayouts/tictac/Players.kt
@@ -0,0 +1,6 @@
+package com.sammy.customlayouts.tictac
+
+sealed class Players {
+    object Player1 : Players()
+    object Player2 : Players()
+}
---
+++ app/src/main/java/com/sammy/customlayouts/tictac/TicTacToe.kt
@@ -0,0 +1,11 @@
+package com.sammy.customlayouts.tictac
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun TicTacToe(
+    modifier: Modifier = Modifier,
+) {
+
+}
