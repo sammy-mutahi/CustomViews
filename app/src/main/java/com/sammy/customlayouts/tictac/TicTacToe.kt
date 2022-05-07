dbb6e0d26637cd8f7c92b0b08baadacc0a83221f
sammy-mutahi <sammymutahi21@gmail.com>
2022-05-07 13:08:58 +0300

    add translational offsets

--- app/src/main/java/com/sammy/customlayouts/tictac/TicTacToe.kt
+++ app/src/main/java/com/sammy/customlayouts/tictac/TicTacToe.kt
@@ -1,15 +1,20 @@
 package com.sammy.customlayouts.tictac
 
+import androidx.compose.foundation.Canvas
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
 import androidx.compose.ui.graphics.vector.PathParser
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
 import com.sammy.customlayouts.R
 
 @Composable
 fun TicTacToe(
     modifier: Modifier = Modifier,
+    distanceBetween: Dp = 50.dp,
+    pathScaleFactor: Float = 7f
 ) {
     var center by remember {
         mutableStateOf(Offset.Unspecified)
@@ -45,6 +50,42 @@ fun TicTacToe(
     }
     val xLeftDiagonalPath = remember {
         PathParser().parsePathString(xLeftDiagonalPathString).toPath()
+    }
+
+    //path bounds
+    val firstRowPathBound = remember {
+        firstRowPath.getBounds()
+    }
+    val secondRowPathBound = remember {
+        secondRowPath.getBounds()
+    }
+    val firstColumnPathBound = remember {
+        firstColumnPath.getBounds()
+    }
+    val secondColumnPathBound = remember {
+        secondColumnPath.getBounds()
+    }
+
+    //translationalOffsets
+    var firstRowTranslationalOffset by remember {
+        mutableStateOf(Offset.Zero)
+    }
+    var secondRowTranslationalOffset by remember {
+        mutableStateOf(Offset.Zero)
+    }
+
+    var firstColumnTranslationalOffset by remember {
+        mutableStateOf(Offset.Zero)
+    }
+    val secondColumnTranslationalOffset by remember {
+        mutableStateOf(Offset.Zero)
+    }
+
+
+
+    Canvas(modifier = modifier) {
+        center = this.center
+
     }
 
 
