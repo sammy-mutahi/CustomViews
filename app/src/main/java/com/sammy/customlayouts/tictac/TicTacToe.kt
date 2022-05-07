08faf2a6284641acd23dfb8251223d2e8a000e47
sammy-mutahi <sammymutahi21@gmail.com>
2022-05-07 12:58:28 +0300

    add real paths

--- app/src/main/java/com/sammy/customlayouts/tictac/TicTacToe.kt
+++ app/src/main/java/com/sammy/customlayouts/tictac/TicTacToe.kt
@@ -3,6 +3,7 @@ package com.sammy.customlayouts.tictac
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.vector.PathParser
 import androidx.compose.ui.res.stringResource
 import com.sammy.customlayouts.R
 
@@ -22,6 +23,29 @@ fun TicTacToe(
     val circlePathString = stringResource(id = R.string.circle_path)
     val xRightDiagonalPathString = stringResource(id = R.string.x_right_diagonal)
     val xLeftDiagonalPathString = stringResource(id = R.string.x_left_diagonal_path)
+
+    //real paths
+    val firstRowPath = remember {
+        PathParser().parsePathString(firstRowPathString).toPath()
+    }
+    val secondRowPath = remember {
+        PathParser().parsePathString(secondRowPathString).toPath()
+    }
+    val firstColumnPath = remember {
+        PathParser().parsePathString(firstColumnPathString).toPath()
+    }
+    val secondColumnPath = remember {
+        PathParser().parsePathString(secondColumPathString).toPath()
+    }
+    val circlePath = remember {
+        PathParser().parsePathString(circlePathString).toPath()
+    }
+    val xRightDiagonalPath = remember {
+        PathParser().parsePathString(xRightDiagonalPathString).toPath()
+    }
+    val xLeftDiagonalPath = remember {
+        PathParser().parsePathString(xLeftDiagonalPathString).toPath()
+    }
 
 
 }
