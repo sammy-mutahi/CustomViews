169f2a84ffc17df283939fa5bd91ec9187828321
sammy-mutahi <sammymutahi21@gmail.com>
2022-05-25 17:12:26 +0300

    add tower of hanoi activity and file

---
+++ app/src/main/java/com/sammy/customlayouts/paths/towerofhanoi/TowerOfHanoiActivity.kt
@@ -0,0 +1,14 @@
+package com.sammy.customlayouts.paths.towerofhanoi
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+
+class TowerOfHanoiActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+
+        }
+    }
+}
